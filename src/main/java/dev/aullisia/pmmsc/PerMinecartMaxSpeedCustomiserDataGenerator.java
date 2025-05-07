d974d04821e4bc63840556722b8bb9c78bab8493
aul <aullisia.dev@gmail.com>
2025-05-07 19:01:02 +0200

    Project Files

---
+++ src/main/java/dev/aullisia/pmmsc/PerMinecartMaxSpeedCustomiserDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.aullisia.pmmsc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PerMinecartMaxSpeedCustomiserDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
